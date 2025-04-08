13f5236c0901581cfd58598943fb29fea139bee7
Nick Stewart <nick@nickstewart.me>
2025-04-07 23:45:18 -0400

    feat: Calculate tariff fees in cart

--- src/Loader.php
+++ src/Loader.php
@@ -2,6 +2,7 @@
 
 namespace TariffFee;
 
+use League\Csv\Reader;
 use Jenssegers\Blade\Blade as Blade;
 
 class Loader {
@@ -9,6 +10,7 @@ class Loader {
 
 	public $pluginBaseDir = null;
 	public $pluginBaseUrl = null;
+	public $tariffData = [];
 
 	public function __construct($pluginBaseDir, $pluginBaseUrl) {
 		$this->pluginBaseDir = $pluginBaseDir;
@@ -24,12 +26,26 @@ class Loader {
 	}
 
 	public function init() {
+		$this->initTariffData();
 		$this->initActions();
+	}
+
+	public function initTariffData() {
+		// Tariff fees are stored in a CSV file for easy updating
+		try {
+			$csv = Reader::createFromPath(__DIR__ . '/resources/data/tariff_fees.csv', 'r');
+			$csv->setHeaderOffset(0);
+			$records = $csv->getRecords();
+			$this->tariffData = $records;
+		} catch (\Exception $e) {
+			//
+		}
 	}
 
 	public function initActions() {
 		add_action('woocommerce_product_options_advanced', [$this, 'displayCountryOfOriginField'], 10);
 		add_action('woocommerce_process_product_meta', [$this, 'saveCountryOfOriginField'], 10, 1);
+		add_action('woocommerce_cart_calculate_fees', [$this, 'addTariffFeeToCart'], 10, 1);
 	}
 
 	/**
@@ -61,5 +77,58 @@ class Loader {
 		if (isset($_POST['tariff_fee_country_of_origin'])) {
 			update_post_meta($id, 'tariff_fee_country_of_origin', $_POST['tariff_fee_country_of_origin']);
 		}
+	}
+
+	/**
+	 * Add tariff fee to the order
+	 */
+	public function addTariffFeeToCart($cart) {
+		// Loop through cart items to calculate product tariff fees from country of origin
+		$all_tarrif_fees = [];
+
+		foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
+			$product = $cart_item['data'];
+			$country_of_origin = $product->get_meta('tariff_fee_country_of_origin', true);
+			$tariff_percent = $this->findTariffFee($country_of_origin);			
+
+			if ($tariff_percent == 0) {
+				continue;
+			}
+
+			$tariff_percent = $tariff_percent / 100;			
+			$tariff_fee = round($product->get_price() * $tariff_percent, 2);
+
+			$all_tarrif_fees[$country_of_origin][] = $tariff_fee;
+		}
+
+		// Add all tariff fees to the cart
+		foreach ($all_tarrif_fees as $country_of_origin => $tariff_fees) {
+			$countries_obj = new \WC_Countries();
+			$countries = $countries_obj->__get('countries');	
+			$country_name = $countries[$country_of_origin];
+
+			$cart->add_fee('Tariff Fee: ' . $country_name, array_sum($tariff_fees));
+		}
+	}
+
+	/**
+	 * Find tariff fee for a country
+	 */
+	public function findTariffFee($country_code): float {
+		$tariffs = $this->tariffData;
+		$tariff_fee = 0;
+
+		if (empty($tariffs)) {
+			return $tariff_fee;
+		}
+
+		foreach ($tariffs as $tariff) {
+			if ($tariff['country'] === $country_code) {
+				$tariff_fee = $tariff['fee'];
+				break;
+			}
+		}
+
+		return $tariff_fee;
 	}
 }
